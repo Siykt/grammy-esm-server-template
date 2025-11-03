d6cc4b88bbb24acada13c8473d6a9c4a749240fb
Siykt <cnsiykt@163.com>
2025-11-03 14:48:49 +0800

    refactor: comment out unused exchange adapters in exchanges.bootstrap.ts

--- src/services/exchange/exchanges.bootstrap.ts
+++ src/services/exchange/exchanges.bootstrap.ts
@@ -2,15 +2,15 @@ import type { ExchangeService } from './exchange.service.js'
 import { BinanceAdapter } from './adapters/binance.adapter.js'
 import { BitgetAdapter } from './adapters/bitget.adapter.js'
 import { BybitAdapter } from './adapters/bybit.adapter.js'
-import { KucoinAdapter } from './adapters/kucoin.adapter.js'
-import { MexcAdapter } from './adapters/mexc.adapter.js'
+// import { KucoinAdapter } from './adapters/kucoin.adapter.js'
+// import { MexcAdapter } from './adapters/mexc.adapter.js'
 import { OkxAdapter } from './adapters/okx.adapter.js'
 
 export function registerDefaultExchangeAdapters(exchangeService: ExchangeService): void {
   exchangeService.registerAdapter(new BinanceAdapter())
   exchangeService.registerAdapter(new OkxAdapter())
   exchangeService.registerAdapter(new BybitAdapter())
-  exchangeService.registerAdapter(new MexcAdapter())
-  exchangeService.registerAdapter(new KucoinAdapter())
+  // exchangeService.registerAdapter(new MexcAdapter())
+  // exchangeService.registerAdapter(new KucoinAdapter())
   exchangeService.registerAdapter(new BitgetAdapter())
 }
